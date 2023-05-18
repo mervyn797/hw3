d93bf42d00cddbddbdc1adeb4983b6b3a3646b44
Mervyn Pereira <mervyn.pereira@kellogg.northwestern.edu>
2023-05-18 00:10:37 +0000

    sync2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "places", :action => "index" })
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-resources "places"
-resources "posts"
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  resources "places"
+  resources "posts"
+  
 end
+
+
