b8a2e39e6cac3cd13e030411ee16d20258fb00c6
Mervyn Pereira <mervyn.pereira@kellogg.northwestern.edu>
2023-05-18 00:33:32 +0000

    sync5

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,15 @@ class PlacesController < ApplicationController
     @place = Place.find_by({"id"=>params["id"]})
   end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
+  end
+
 end
